c76d42ef8ad3d976ccdcf6506bb6095a3c59a6bd
Thiago <thiagoodemedeiros@gmail.com>
2023-11-15 22:44:42 -0300

    fix: add fullPortrait in AgentProps

--- src/components/Agent/index.tsx
+++ src/components/Agent/index.tsx
@@ -11,17 +11,18 @@ interface AgentProps{
       displayName: string
       displayIcon: string
     }[] //além de obj é um array
+    fullPortrait: string
   }
 }
 
 export const Agent = ({ agent }: AgentProps) => (
   <a href="#" className={styles.agent}>
-        <div>
+        <div className={styles.text}>
           <p>{agent.role.displayName}</p>
           <strong>{agent.displayName}</strong>
         </div>
 
-        <ul>
+        <ul className={styles.abilities}>
           {agent.abilities.map(ability => (
             <li key={ability.displayName}>
               <Image 
@@ -33,5 +34,9 @@ export const Agent = ({ agent }: AgentProps) => (
             </li>
           ))}
         </ul>
+
+        <div className={styles.background}>
+            <span style={{backgroundImage: `url('${agent.fullPortrait}')`}}/>
+        </div>
       </a>
 )
